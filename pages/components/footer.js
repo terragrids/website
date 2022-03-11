9c125399c87a22e5dd80643f8c4f38d212470461
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-03-11 09:44:06 +0000

    Add algorand links in footer

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -4,17 +4,31 @@ import Section from './section'
 import styles from './footer.module.scss'
 
 export default function Footer() {
+    const algorandUrl = 'https://www.algorand.com/about/sustainability';
     return (
         <Section classNames={[styles.container]}>
             <div className={styles.poweredBy}>
                 {strings.poweredBy}
                 <div className={styles.algorand}>
-                    <Image
-                        src="/images/algorand-logo-white.png"
-                        layout={'fill'}
-                        objectFit={'contain'}
-                        alt='Algorand' />
+                    <a
+                        href={algorandUrl}
+                        target={'_blank'}
+                        rel={'noreferrer'}>
+                        <Image
+                            src="/images/algorand-logo-white.png"
+                            layout={'fill'}
+                            objectFit={'contain'}
+                            alt='Algorand' />
+                    </a>
                 </div>
+            </div>
+            <div className={styles.sub}>
+                <a
+                    href={algorandUrl}
+                    target={'_blank'}
+                    rel={'noreferrer'}>
+                    {strings.carbonNegativeBlockchain}
+                </a>
             </div>
         </Section>
     )
--- strings/en.js
+++ strings/en.js
@@ -8,5 +8,6 @@ export const strings = {
     whatIsTerragrids2: 'Participants in the Terragrids ecosystem (called citizens) can buy digital crypto tokens (NFTs) that have a 1:1 link with unique solar PV cells in the real world. Participants’ tokens make up virtual solar power systems in the Terragrids greenverse. These virtual systems are linked to real solar PV systems installed on the roofs of real people that live in fuel poverty.',
     whatIsTerragrids3: 'Anyone can become a Terragrids citizen. You just need an Algorand Wallet and a passion for sustainability and decentralisation. Terragrids citizens can own, explore, use and trade their tokens to help govern and expand a virtual electricity grid that power towns and villages in the greenverse; most importantly, they help build and expand a real electricity generation grid, which will reduce fuel poverty, help meet global carbon reduction targets, and ensure a better future for the planet.',
     contributeToWhitePaper: 'View and contribute to the whitepaper',
-    poweredBy: 'Powered by'
+    poweredBy: 'Powered by',
+    carbonNegativeBlockchain: 'carbon negative blockchain'
 }
