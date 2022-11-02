4e2c3b9eb36d1b5ea5ba917811033fd731737f88
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-11-02 15:54:39 +0000

    Update splash text

--- strings/en.js
+++ strings/en.js
@@ -8,26 +8,31 @@ export const strings = {
     contributeToWhitePaper: 'View and contribute to the whitepaper',
     current: 'Current',
     decentralized: 'Decentralized',
-    decentralizedContent: 'Terragrids is decentralized from inception. The whitepaper is a collaborative document where anyone can comment and propose new ideas. The website and the DApp are entirely open source, so that any developers across the globe can contribute to making Terragrids a better platform. Terragrids Backers can acquire DAO governance rights, and they can participate, discuss and vote for internal decisions.',
+    decentralizedContent:
+        'Terragrids is decentralized from inception. The whitepaper is a collaborative document where anyone can comment and propose new ideas. The website and the DApp are entirely open source, so that any developers across the globe can contribute to making Terragrids a better platform. Terragrids Backers can acquire DAO governance rights, and they can participate, discuss and vote for internal decisions.',
     draftTokenomics: 'First draft of tokenomics for the DAO',
     electron: 'Electron',
     founderDesign: 'Founder & Design Lead',
     founderTech: 'Founder & Tech Lead',
     governance: 'Governance',
-    governanceContent1: 'Terragrids is building a new concept for funding renewable energy projects at scale. Rather than relying on single individuals to foot the bill, Terragrids seeks to spread the financial investment across the globe. The Terragrids platform provides an online gamified experience for Backers to invest as little as $10 to buy Non-Fungible Tokens on the energy-efficient Algorand blockchain and build their sustainable towns and communities in the metaverse. Revenues from the sales are placed in a Terragrids treasury Algo crypto-wallet, that everyone can see. A small part of this money is used to sustain the business, while most of the revenue is used to fund Creators\' renewable energy projects.',
-    governanceContent2: 'Terragrids is being set up as a Decentralised Autonomous Organisation (DAO). Backers holding at least one NFT can be part of the DAO governing body, monitoring and contributing to its operations. They can also see in detail how their purchase of NFTs in the metaverse directly financed real assets on the ground, thanks to a project tracker also built on the Agorand blockchain.',
+    governanceContent1:
+        "Terragrids is building a new concept for funding renewable energy projects at scale. Rather than relying on single individuals to foot the bill, Terragrids seeks to spread the financial investment across the globe. The Terragrids platform provides an online gamified experience for Backers to invest as little as $10 to buy Non-Fungible Tokens on the energy-efficient Algorand blockchain and build their sustainable towns and communities in the metaverse. Revenues from the sales are placed in a Terragrids treasury Algo crypto-wallet, that everyone can see. A small part of this money is used to sustain the business, while most of the revenue is used to fund Creators' renewable energy projects.",
+    governanceContent2:
+        'Terragrids is being set up as a Decentralised Autonomous Organisation (DAO). Backers holding at least one NFT can be part of the DAO governing body, monitoring and contributing to its operations. They can also see in detail how their purchase of NFTs in the metaverse directly financed real assets on the ground, thanks to a project tracker also built on the Agorand blockchain.',
     governanceDefined: 'DAO governance defined',
     grid: 'Grid',
     howAreWeDoingThis: 'How are we doing this?',
     idoAllocation: 'ICO / IDO for round of company funding',
-    ireneBio: 'Hello! I am Irene, an illustrator, graphics desiger and art director with 15+ years of experience in the industry.\nI am a member of the British Association of Illustrators (AOI) and of the Society of Children’s Book Writers and Illustrators (SCBWI).\nApart from drawing, I love cooking, reading, photography, walking and spending time in nature with my family.\nI am the founder of Editartz (https://editartz.com), an online creative community of illustrators from around the world that regularly share their editorial, children’s and pattern design work.\nFrom October 2020 to December 2021, I led "Planet Posters", an art project in partnership with One Planet Conservation Awareness - 12 months’ focus on the environment: https://www.editartz.com/projects/planet-posters.\nFollowing my passion for nature conservation and sustainability, I founded Terragrids as the Design Lead.',
+    ireneBio:
+        'Hello! I am Irene, an illustrator, graphics desiger and art director with 15+ years of experience in the industry.\nI am a member of the British Association of Illustrators (AOI) and of the Society of Children’s Book Writers and Illustrators (SCBWI).\nApart from drawing, I love cooking, reading, photography, walking and spending time in nature with my family.\nI am the founder of Editartz (https://editartz.com), an online creative community of illustrators from around the world that regularly share their editorial, children’s and pattern design work.\nFrom October 2020 to December 2021, I led "Planet Posters", an art project in partnership with One Planet Conservation Awareness - 12 months’ focus on the environment: https://www.editartz.com/projects/planet-posters.\nFollowing my passion for nature conservation and sustainability, I founded Terragrids as the Design Lead.',
     kickstartFunding: 'Kickstart funding campaign planned',
     kickstartLaunched: 'Kickstart funding campaign launched',
     linkedIn: 'LinkedIn',
-    lorenzoBio: 'Hello! I am Lorenzo, a software engineering professional with 20+ years of experience in a large number of different software technologies, programming languages and device platforms.\nI have managed and led development teams as Tech Lead, Engineering Manager and Technical Programme Manager, focusing on delivery, performance and quality metrics.\nI have worked on projects from startups to large corporations at every level of the software stack, from real-time hardware adaptation to backend, frontend, and mobile application development.\nI am passionate about making things happen, from product ideas to customer adoption, always with a “lean-startup” entrepreneurial mindset.\nI have always had a strong interest in renewable energy, sustainable development and tackling climate change. Combining this with my recent interest in blockchain technologies and decentralised applications, I founded Terragrids to help preserve the world’s climate and take concrete actions for those most affected by fuel poverty.',
+    lorenzoBio:
+        'Hello! I am Lorenzo, a software engineering professional with 20+ years of experience in a large number of different software technologies, programming languages and device platforms.\nI have managed and led development teams as Tech Lead, Engineering Manager and Technical Programme Manager, focusing on delivery, performance and quality metrics.\nI have worked on projects from startups to large corporations at every level of the software stack, from real-time hardware adaptation to backend, frontend, and mobile application development.\nI am passionate about making things happen, from product ideas to customer adoption, always with a “lean-startup” entrepreneurial mindset.\nI have always had a strong interest in renewable energy, sustainable development and tackling climate change. Combining this with my recent interest in blockchain technologies and decentralised applications, I founded Terragrids to help preserve the world’s climate and take concrete actions for those most affected by fuel poverty.',
     mediumFirstPost: 'First blog post on Medium',
     mission: 'Mission',
-    newCreatorsProjects: 'More Testnet creator\'s signed up with new projects',
+    newCreatorsProjects: "More Testnet creator's signed up with new projects",
     newNFTs: 'New NFTs designed',
     openSourceSetUp: 'Open source organisation set up on GitHub (website + dapp)',
     ourMission: 'Our Mission',
@@ -37,7 +42,7 @@ export const strings = {
     roadmap: 'Roadmap',
     siteTitle: 'Terragrids',
     socialCommunities: 'Communities on Discord, Twitter and GitHub set up',
-    splashDescription: 'Energy for everyone. Built on the blockchain.',
+    splashDescription: 'Energy for everyone. Built on Algorand blockchain.',
     splashTitle: 'Crowdfunding for renewable energy projects',
     team: 'The team',
     team1: 'Team inception',
@@ -48,15 +53,21 @@ export const strings = {
     webappDesign: 'Web app + dapp design + POC',
     webappPrototype: 'Web app + dapp prototype developed and showcased on website',
     webAppV1: 'Web app + dapp prototype improved to include map, NFTs (land, solar cells, buildings)',
-    webAppV2: 'Web app + dapp to include creator\'s account, project land allocation, draft project tracker',
-    webAppV3: 'Web app + dapp to include full creator\'s project tracker, DAO governance tools, internal secondary marketplace for NFTs',
+    webAppV2: "Web app + dapp to include creator's account, project land allocation, draft project tracker",
+    webAppV3:
+        "Web app + dapp to include full creator's project tracker, DAO governance tools, internal secondary marketplace for NFTs",
     website1_0: 'Website 1.0',
     website2_0: 'Website 2.0',
-    whatIsTerragrids1: 'Terragrids is a decentralised crowdfunding platform whose goal is to empower communities and individuals to take back ownership of what nature gives them for free: energy.',
-    whatIsTerragrids2: 'Terragrids provides a funding platform for renewable energy projects. The platform runs a small metaverse, where project Creators are allocated part of the metaverse estate, and other participants - called Backers - can explore, build and develop sustainable towns on the Creators\' land.',
-    whatIsTerragrids3: 'Backers buy NFTs and develop towns on Creators\' land in the virtual map. The revenues from the purchase of each NFT will be administered by Terragrids in a treasury Algorand wallet and will be allocated to each Creator\'s account to fund the budget goal set for their renewable energy project.',
-    whatIsTerragrids4: 'NFTs will give Backers governance rights in the management of the Terragrids metaverse, gradually developing Terragrids into a Decentralised Autonomous Organisation (DAO).',
-    whatIsTerragrids5: 'Anyone can become a Creator or a Backer on Terragrids. They just need an Algorand Wallet and a passion for sustainability and decentralisation. Creators can set up a project brief, a budget goal, and associate it to an area of land on Terragrids open for crowdfunding. Backers can buy, collect and trade their NFTs to expand virtual towns and villages in the greenverse; doing so, they fund real renewable energy projects, which will help meet global carbon reduction targets and ensure a better future for the planet.',
+    whatIsTerragrids1:
+        'Terragrids is a decentralised crowdfunding platform whose goal is to empower communities and individuals to take back ownership of what nature gives them for free: energy.',
+    whatIsTerragrids2:
+        "Terragrids provides a funding platform for renewable energy projects. The platform runs a small metaverse, where project Creators are allocated part of the metaverse estate, and other participants - called Backers - can explore, build and develop sustainable towns on the Creators' land.",
+    whatIsTerragrids3:
+        "Backers buy NFTs and develop towns on Creators' land in the virtual map. The revenues from the purchase of each NFT will be administered by Terragrids in a treasury Algorand wallet and will be allocated to each Creator's account to fund the budget goal set for their renewable energy project.",
+    whatIsTerragrids4:
+        'NFTs will give Backers governance rights in the management of the Terragrids metaverse, gradually developing Terragrids into a Decentralised Autonomous Organisation (DAO).',
+    whatIsTerragrids5:
+        'Anyone can become a Creator or a Backer on Terragrids. They just need an Algorand Wallet and a passion for sustainability and decentralisation. Creators can set up a project brief, a budget goal, and associate it to an area of land on Terragrids open for crowdfunding. Backers can buy, collect and trade their NFTs to expand virtual towns and villages in the greenverse; doing so, they fund real renewable energy projects, which will help meet global carbon reduction targets and ensure a better future for the planet.',
     whitepaper: 'Whitepaper',
     wire: 'Wire'
 }
