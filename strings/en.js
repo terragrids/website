d2f1579c795d9231a3be4d49b6ab5a041919bf10
Lorenzo Polidori <lorenzopolidori@gmail.com>
2023-04-26 12:17:47 +0100

    Minor content changes

--- strings/en.js
+++ strings/en.js
@@ -19,7 +19,7 @@ export const strings = {
         'Terragrids is building a new concept for funding renewable energy projects at scale. Rather than relying on single individuals to foot the bill, Terragrids seeks to spread the financial investment across the globe. \nThe Terragrids platform provides an online gamified experience for Backers to invest as little as $10 to buy Non-Fungible Tokens on the energy-efficient Algorand blockchain and create sustainable towns in the Terragrids virtual world. \nRevenues from the sales of NFTs are transparently managed by Algorand Smart Contracts. In a single transaction, a small part of this money is used to sustain the business, a royalty is paid to the NFT illustrators, and most of the revenue is used to support the Creators to achive their project goals.',
     governanceDefined: 'DAO governance defined',
     grid: 'Grid',
-    howAreWeDoingThis: 'A platform for smart-home owners',
+    howAreWeDoingThis: 'The Platform for Smart Home Owners',
     idoAllocation: 'ICO / IDO for round of company funding',
     ireneBio:
         'Hello! I am Irene, an illustrator, graphics designer and art director with 15+ years of experience in the industry.\nI am a member of the British Association of Illustrators (AOI) and of the Society of Children’s Book Writers and Illustrators (SCBWI).\nI am the founder of editartz (https://editartz.com), an online creative community of illustrators from around the world that regularly share their editorial, children’s and pattern design work.\nFrom October 2020 to December 2021, I led "Planet Posters", an art project in partnership with One Planet Conservation Awareness - 12 months’ focus on the environment: https://www.editartz.com/projects/planet-posters.\nApart from drawing, I love cooking, reading, photography, walking and spending time in nature with my family.\nFollowing my passion for nature conservation and sustainability, I co-founded Terragrids as the Design Lead.\nFind more about me at www.irenesilvino.com',
@@ -50,13 +50,13 @@ export const strings = {
     website1_0: 'Website 1.0',
     website2_0: 'Website 2.0',
     whatIsTerragrids1:
-        'Terragrids is a web3 decentralised energy tracking platform for smart homes whose goal is to empower individuals to take back ownership of what nature gives them for free: energy.',
+        'Terragrids is a web3 decentralised energy tracking platform for smart homes, built to empower individuals to take back ownership of what nature gives them for free: energy.',
     whatIsTerragrids2:
-        'The platform runs a virtual world, where home owners are allocated part of the estate, and given a token on the blockchain to uniquely identify their place.',
+        'Terragrids runs a virtual world, where home owners are allocated part of the estate, and given a token on the blockchain to uniquely identify their place.',
     whatIsTerragrids3:
-        'Home owners can develop their place adding trackers which monitor the energy balance of their smart home: consumption, renewable generation, electric vehicles, heat pumps performance, etc.',
+        'Home owners can develop their place adding trackers which monitor the energy balance of their smart home: consumption, renewable generation, electric vehicles, heat pump performance, etc.',
     whatIsTerragrids4:
-        'All data flows coming from the trackers are saved with unique, tamper-proof transactions on the fastest and greenest blockchain: Algorand.',
+        'All data flows coming from the trackers are saved using unique, tamper-proof transactions on the fastest and greenest blockchain: Algorand.',
     whatIsTerragrids5:
         'Home owners will be able to share this data as proof of quality for their smart home and integrate with the energy network of the future.',
     whitepaper: 'Whitepaper',
